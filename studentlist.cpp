222546e70a932c24f0300ffdc7273c2d08fd628d
juststn <42872030+juststn@users.noreply.github.com>
2018-10-05 08:25:03 -0700

    Update studentlist.cpp

--- studentlist.cpp
+++ studentlist.cpp
@@ -11,7 +11,14 @@ struct student{
 };
 
 int main(){
+  char firstname [50][50];
+  char lastname [50][50];
+  int id [50][50];
+  float GPA [50][50];
+
+  string input;
+  cout<<"hi"<<endl;
+  cin>>input;
+  cout<<input<<endl;
 
 }
-
-
