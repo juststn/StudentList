bf0585199e2b1ec6bc01a3b298a8e78c2c2e1afd
juststn <42872030+juststn@users.noreply.github.com>
2018-10-11 09:06:38 -0700

    Update studentlist.cpp

--- studentlist.cpp
+++ studentlist.cpp
@@ -7,18 +7,28 @@
 using namespace std;
 
 struct student{
+  char firstname [50];
+  char lastname [50];
+  int id;
+  float GPA;
 
 };
 
 int main(){
-  char firstname [50][50];
-  char lastname [50][50];
-  int id [50][50];
-  float GPA [50][50];
-
+  vector<student*>* studentList;
+  studentList = new(vector<student*>);
+  char input[20];
   string input;
-  cout<<"hi"<<endl;
-  cin>>input;
-  cout<<input<<endl;
+  int running = 1;
+  int count =0;
+  int studentnumber;
+  int deletedStudent = 0;
+
+  while(running = 1){
+    cout<<"Enter studentnumber, gpa, annd name"<<endl;
+    cin>>input;
+    cout<<input<<endl;
+
+  }
 
 }
