f5d56ae0313cc3ccfa9a84443e3c438621d63acb
juststn <42872030+juststn@users.noreply.github.com>
2018-10-25 22:52:34 -0700

    Add files via upload

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
 
+
+//Student List by Justin He -
+//Organize students with commands ADD, PRINT, DELETE, and QUIT
+//ADD prompts input for name, id, gpa
+//PRINT prints out every student currently stored
+//DELETE prompts input to delete entry by student id
+//QUIT exits the program
+
+#include <iostream>
 #include <string.h>
 #include <iomanip>
 #include <vector>
@@ -8,75 +16,112 @@
 using namespace std;
 
 struct student{
-  char firstname [50];
-  char lastname [50];
-  int id;
+  char firstname[50]; //initialize struct
+  char lastname[50];
+  int  id;
   float GPA;
 
 };
 
-void addStudent();
-void printStudent();
-void deleteStudent();
+void addStudent(vector<student*>*); //define functions
+void printStudent(vector<student*>*);
+void deleteStudent(vector<student*>*);
+
 
 int main(){
-  vector<student*>* studentList;
-  studentList = new(vector<student*>);
-  char input[20];
+
   string in;
   int running = 1;
-  int count =0;
-  int studentnumber;
-  int deletedStudent = 0;
-
+  
+  vector<student*>* studentList = NULL;
+  studentList = new vector<student*>;
+  
   while(running == 1){
-
+    
     cout<<"enter command:[ADD,PRINT,DELETE,QUIT] "<<endl;
     cin>>in;
     if(in=="ADD"){
-      cout<<"add first name"<<endl;
-      addStudent();
+      addStudent(studentList);
     }
     if(in=="PRINT"){
-      cout<<"printing all students"<<endl;
-      printStudent();
+      printStudent(studentList);
     }
     if(in=="DELETE"){
-      cout<<"which id to remove?"<<endl;
-      deleteStudent();
+      deleteStudent(studentList);
     }
     if(in=="QUIT"){
       running = 0;
     }
+    
+  }
+}
 
-    /*
-    cout<<"Enter studentnumber"<<endl;
-   cin>>in;
-    id = studentnumber;
-    cout<<"enter first name"<<endl;
-    cin>>in;
-    in = firstname[count];
-    cout<<"enter last name"<<endl;
-    cin>>in;
-    in = lastname[count];
-    cout<<"enter gpa"<<endl;
-    cin>>in;
-    in = GPA;
-    count++;*/
+// add a student
+void addStudent(vector<student*>* studentList) {
+
+    student* oneStudent;
+    oneStudent = new(student);
+    
+    cout<<"Enter first name: ";
+    cin >> oneStudent->firstname;
+    
+    cout<<"Enter last name: ";
+    cin >> oneStudent->lastname;
+
+    cout<<"Enter student id (a number): ";
+    cin>> oneStudent->id;
+    cin.ignore();
+    
+    cout<<"Enter gpa: ";
+    cin>> oneStudent->GPA;
+    cin.ignore();
+
+    //add inputs into vector
+    studentList->push_back(oneStudent);
+    cout << "Student added." << endl << endl;
+}
+
+// print student list
+void printStudent( vector<student*>* studentList) {
+
+  cout << "Total number of students: " << studentList->size() << endl;
+
+  //if student size is 0 return
+  if(studentList->size() == 0){
+    return;
   }
 
+  //iterate through vector and output name, id, GPA
+  for (vector<student*>::iterator v = studentList->begin(); v < studentList->end(); v++) {
+    cout << (*v)->firstname << " " << (*v)->lastname << ", ";
+    cout << (*v)->id << ", ";
 
+    // set precision to 2 digits after the decimal point
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << (*v)->GPA << endl;
+   }
 
+  cout << endl;
 }
 
-void addStudent()  {
+// delete a student from the vector
 
-}
+void deleteStudent(vector<student*>* studentList) {
+  int input;
+  cout<<"which student id should be deleted?" << endl;
+  cin >> input;
+  cin.ignore();
+  //loop through vector to find id and erase/delete it
+  for(vector<student*>::iterator i = studentList->begin(); i<studentList->end(); i++)
+    {
+      if ((*i)->id == input){
+	delete (*i);
+	studentList->erase(i);
+	i--;
+      }
+    }
 
-void printStudent() {
-
-}
-
-void deleteStudent() {
-
+  cout << "Student deleted." << endl << endl;
+  
 }
